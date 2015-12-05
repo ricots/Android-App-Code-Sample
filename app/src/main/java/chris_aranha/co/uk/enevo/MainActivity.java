f04ccad68f08e92489a864f8fbed7b3c4ecd1263
Chris Aranha <development@chris-aranha.co.uk>
2015-12-05 15:53:34 +0000

    Initial Commit

---
+++ app/src/main/java/chris_aranha/co/uk/enevo/MainActivity.java
@@ -0,0 +1,13 @@
+package chris_aranha.co.uk.enevo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
