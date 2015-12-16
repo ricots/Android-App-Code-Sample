54137c20d984d3185461750d4ef5d8e1b0a8fcaa
Chris Aranha <development@chris-aranha.co.uk>
2015-12-16 01:47:41 +0000

    Created Utility Class with Constants

---
+++ app/src/main/java/chris_aranha/co/uk/enevo/Utils.java
@@ -0,0 +1,9 @@
+package chris_aranha.co.uk.enevo;
+
+public class Utils {
+    public static final String DOMAIN = "http://chris-aranha.info";
+    public static final String ENDPOINT = "/restapi";
+    public static final String PHOTOUNITLISTING = "/photo_unit_listing";
+    public static final String IMAGEFILEFOLDERLOCATION = "/sites/default/files";
+    public static final String ITEM = "/node/";
+}
